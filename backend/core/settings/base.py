9f99e723bde477154f8586796af422f798c043ae
hudaakhter09-cloud <hudaakhter09@gmail.com>
2026-07-02 17:02:11 +0100

    fixed deployment issues

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -141,6 +141,9 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
     }
 }
 
